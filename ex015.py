2b84a601a06fc7a413aff8e242101d77f276d136
Cacula10 <flaviodesouzamarques@gmail.com>
2019-10-31 23:02:33 -0300

    update

--- ex015.py
+++ ex015.py
@@ -1,1 +1,4 @@
-###
+dias = int(input('Quantos dias alugados ?'))
+km = float(input('Quantos km rodados ? '))
+total = dias * 60 + (0.15 * km)
+print('O total a pagar por {} dias e {}km rodados é: R${:.2f}'.format(dias, km, total))
