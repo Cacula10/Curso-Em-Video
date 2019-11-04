a188ae237f05e3f422cd8f72e0abde80c5ba4f8b
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-04 19:51:09 -0200

    Concluido

--- ex023.py
+++ ex023.py
@@ -1,3 +1,16 @@
 n = int(input('Informe um numero: '))
 print('Analisando o numero {}'.format(n))
-print('Unidade {} Dezena {} Centena {} Milhar {}'.format((), (n/10), (n/100), (n/1000)))
+if n < 10:
+    print('Unidade {}'.format(n))
+if n >= 10 and n < 100:
+    print('Unidade {}'.format(n % 10))
+    print('Dezena  {}'.format(n // 10))
+if n>= 100 and n < 1000:
+    print('Unidade {}'.format(n % 100 % 10))
+    print('Dezena  {}'.format(n % 100 // 10))
+    print('Centena {}'.format(n // 100))
+if n >= 1000 and n < 10000:
+    print('Unidade {}'.format(n % 1000 % 10))
+    print('Dezena  {}'.format(n % 1000 // 10 % 10))
+    print('Centena {}'.format(n % 1000 // 100))
+    print('Milhar  {}'.format(n // 1000))
