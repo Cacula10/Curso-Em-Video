05f016029012a7ea91ce1641c6205cd639db6f02
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-04 00:03:33 -0200

    upload

---
+++ ex022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite o seu nome completo: ')).strip()
+separa = nome.split()
+print('Analisando o seu nome...')
+print('Seu nome em maisculo é: {}'.format(nome.upper()))
+print('Seu nome em minusculo é: {}'.format(nome.lower()))
+print('O seu nome ao todo tem {} letras'.format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome é {} e tem {} letras'.format(separa[0], len(separa[0])))
