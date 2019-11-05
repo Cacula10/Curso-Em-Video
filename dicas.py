c3a09a795469f0efebb4a1f463a8df4d17a3869d
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-05 18:33:06 -0200

    + Itens

--- dicas.py
+++ dicas.py
@@ -1,2 +1,5 @@
 # usar o "in" para encontrar algo dentro de uma string (exercicios 24 e 25)
 # usar o [:] se quiser achar algo com uma posição especifica
+# no exercicio 26 não foi explicado como não calcular os espaços dentro da frase
+
+## posso usar dois ou mais métodos no msm objeto
