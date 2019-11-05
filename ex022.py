3a13a338a40d008c340e859dc06238191cb03afe
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-05 18:41:26 -0200

    .

--- ex022.py
+++ ex022.py
@@ -5,3 +5,5 @@ print('Seu nome em maisculo é: {}'.format(nome.upper()))
 print('Seu nome em minusculo é: {}'.format(nome.lower()))
 print('O seu nome ao todo tem {} letras'.format(len(nome)-nome.count(' ')))
 print('Seu primeiro nome é {} e tem {} letras'.format(separa[0], len(separa[0])))
+
+# nesse exercicio eu consigo ignorar os ESPAÇOS pois é uma conta com o uso do len, diferente do exercicio 26
