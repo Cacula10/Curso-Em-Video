bdbb2d03ddc795f0430709954b65696ab0fe8f84
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-06 14:39:24 -0200

    upload

--- ex026.py
+++ ex026.py
@@ -1,4 +1,5 @@
 frase = str(input('Digite uma frase: ')).strip()
 print('A letra A aparece {} vezes na frase'.format(frase.upper().count('A')))
 print('A primeira letra A apareceu na posiçao {}'.format(frase.upper().index('A')+1))
-print('A ultima letra A apareceu na posição {}'.format(frase.upper().rfind('A')+1))# nao considerou o espaço entre a frase
+print('A ultima letra A apareceu na posição {}'.format(frase.upper().rfind('A')+1))
+# nao considerou o espaço entre a frase
---
+++ ex027.py
@@ -0,0 +1,4 @@
+nome = str(input('Informe o seu nome completo, por favor: ')).split()
+print('Muito prazer em te conhecer...')
+print('O seu primeiro nome é: {}'.format(nome[0].capitalize()))
+print('O seu ultimo nome é:   {}'.format(nome[-1].capitalize()))
