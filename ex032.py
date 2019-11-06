20aa7e6bf08a5d277078195663f3f8b277744032
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-06 19:42:26 -0200

    Em Processo....

---
+++ ex032.py
@@ -0,0 +1,5 @@
+print('\033[1;32m*' * 70)
+print('{:^70}'.format('\033[1;34mANÁLISE DE ANO BISSEXTO\033[m'))
+print('\033[1;32m*\033[m' * 70)
+ano = int(input('Informe o ano que deseja que nós validemos ou coloque [0] para analisar o ano atual:  '))
+
