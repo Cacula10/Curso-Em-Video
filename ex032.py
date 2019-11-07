fd0e23606b13947a0d8125c54b29ed0a9ae23f8f
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-07 11:22:17 -0200

    UPDATE

--- ex032.py
+++ ex032.py
@@ -2,4 +2,14 @@ print('\033[1;32m*' * 70)
 print('{:^70}'.format('\033[1;34mANÁLISE DE ANO BISSEXTO\033[m'))
 print('\033[1;32m*\033[m' * 70)
 ano = int(input('Informe o ano que deseja que nós validemos ou coloque [0] para analisar o ano atual:  '))
+if ano % 4 == 0:
+    if ano % 100 != 0:
+        print('BISSEXTO')
+    elif ano % 100 == 0:
+        if ano % 400 == 0:
+            print('BISSEXTO - DIVISAO EXATA POR 400 ')
+        else:
+            print('NÃO É UM ANO BISSEXTO')
+else:
+    print('NÃO É UM ANO BISSEXTO')
 
