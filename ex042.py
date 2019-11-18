f3398f3bf8fe88243e85f5ba052bbdb7d29a5f41
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-18 21:06:20 -0200

    upload

---
+++ ex042.py
@@ -0,0 +1,6 @@
+print('\033[1;31m-=\033[m' * 25)
+print('{:^50}'.format('\033[1;34m Analisando um triangulo\033[m'))
+print('\033[1;31m-=\033[m' * 25)
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
