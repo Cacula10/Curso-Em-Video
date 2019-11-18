5b62badf2a70f92861063af7284076d546e60686
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-18 13:47:01 -0200

    upload

--- ex037.py
+++ ex037.py
@@ -10,15 +10,15 @@ opção = str(input('sua opção: '))[0]
 while opção == '1' or '2' or '3':
     if opção == '1':
         binario = bin(numero)
-        print('CONVERSÃO PARA BINÁRIO: ', '\033[1;34m', str(binario[2:]), '\033[m')
+        print('CONVERTENDO PARA BINÁRIO: ', '\033[1;34m', str(binario[2:]), '\033[m')
         break
     elif opção == '2':
         octal = oct(numero)
-        print('CONVERSÃO PARA OCTAL: ', '\033[1;31m', str(octal[2:]), '\033[m')
+        print('CONVERTENDO PARA OCTAL: ', '\033[1;31m', str(octal[2:]), '\033[m')
         break
     elif opção == '3':
         hexadecimal = hex(numero)
-        print('CONVERSÃO PARA HEXADECIMAL', '\033[1;32m', str(hexadecimal[2:]), '\033[m')
+        print('CONVERTENDO PARA HEXADECIMAL', '\033[1;32m', str(hexadecimal[2:]), '\033[m')
         break
     else:
-        opção = int(input('Opção inválida... Digite um valor entre [1 - 3]'))[0]
+        opção = str(input('Opção inválida... Digite um valor entre [1 - 3]'))[0]
