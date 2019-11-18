6362ed229dea340f7a038908330594b2962a3e54
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-18 20:33:27 -0200

    upload

--- ex040.py
+++ ex040.py
@@ -1,12 +1,16 @@
-nota1 = float(input('Informe a nota 1: '))
-nota2 = float(input('Informe a nota 2: '))
+print('\033[1;32m<>\033[m' * 35)
+print('{:^70}'.format('\033[1;34mSISTEMA DE CONTROLE DE MÉDIA DE ALUNOS\033[m'))
+print('\033[1;32m<>\033[m' * 35)
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
 media = (nota1+nota2)/2
-
+print('')
+print('Tirando na primeira nota \033[1;32m{:.1f}\033[m e \033[1;32m{:.1f}\033[m na segunda\nMédia do aluno foi \033[1;32m{:.1f}\033[m'.format(nota1, nota2, media))
+print('')
 if media < 5:
-    print('Parabens burrão! foi reprovado.')
-elif media < 6.9:
-    print('quase burrão...ficou em recuperação')
+    print('\033[1;36mREPROVADO.\033[m')
+elif media >= 5 and media < 7:
+    print('\033[1;36mRECUPERAÇÃO\033[m')
 else:
-    print('APROVADO!')
+    print('\033[1;32mAPROVADO!\033[m')
 
-print(media)
