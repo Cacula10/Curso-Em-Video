8414ef8404547fe4d317becb72dbe9e399ef939c
Cacula10 <flaviodesouzamarques@gmail.com>
2019-11-19 20:55:26 -0200

    upload

--- dicas.py
+++ dicas.py
@@ -11,3 +11,4 @@
 # para finalizar usamos o \033[-m  somente isso
 
 # ex033 ajuda comparar 3 valores
+# ex044 Ensina a centralizar texto
--- ex044.py
+++ ex044.py
@@ -1,13 +1,25 @@
-produto = float(input('Informe o valor do produto: '))
-condição = str(input('Informe qual será a condição de pagamento: D,C,2x,3x: ')).upper()
+print('{:=^70}'.format('\033[1;34m LOJAS CAÇULA \033[m'))
+produto = float(input('Preço da compra: R$ '))
+print('''\033[1;34mFORMAS DE PAGAMENTO\033[m
+[1] Á VISTA DINHEIRO/CHEQUE
+[2] Á VISTA CARTÃO
+[3] 2X CARTÃO
+[4] 3 X OU MAIS NO CARTÃO
+''')
 
-if condição == 'D':
-    print('DESCONTO R$:{:.2f} - O PRODUTO IRÁ CUSTAR R$: {:.2f}'.format(produto*0.1, produto-(produto*0.1)))
-elif condição == 'C':
-    print('DESCONTO R$: {:.2F} - O PRODUTO IRÁ CUSTAR R$: {:.2F}'.format(produto*0.05,produto - (produto*0.05)))
-elif condição == '2X':
-    print('NÃO POSSUÍ DESCONTO, O VALOR SERÁ O MESMO R$: {:.2F}'.format(produto))
-elif condição == '3X':
-    print('ACRESCIMO DE R$: {:.2F} - O PRODUTO IRÁ CUSTAR R$: {:.2F}'.format(produto*0.2, produto + (produto*0.2)))
+opção = int(input('Qual opção? '))
+if opção == 1:
+    desconto = produto * 10/100
+    print('\033[1;36m[1] Á VISTA DINHEIRO/CHEQUE...\033[m\nProduto: \033[1;34m{:.2f}\033[m\nDesconto: \033[1;34m{:.2f}\033[m\nCusto Final: \033[1;34m{:.2f}\033[m'.format(produto, desconto, produto - desconto))
+elif opção == 2:
+    desconto = produto * 5/100
+    print('\033[1;36m[2] Á VISTA CARTÃO...\033[m\nProduto: \033[1;34m{:.2f}\033[m \nDesconto: \033[1;34m{:.2f}\033[m \nCusto Final: \033[1;34m{:.2f}\033[m '.format(produto, desconto, produto - desconto))
+elif opção == 3:
+    parcelas = produto / 2
+    print('\033[1;36m[3] 2X CARTÃO - \033[1;34mSEM DESCONTO\033[m\n2 Parcelas: \033[1;34m{:.2f}\033[m \nCusto Final \033[1;34m{:.2f}\033[m'.format(parcelas, produto))
+elif opção == 4:
+    acrescimo = produto * 20/100
+    qt_parcelas = int(input('Informe a quantidade de parcelas: '))
+    print('\033[1;34m[4] 3 X OU MAIS NO CARTÃO...\033[m \nProduto: \033[1;34m{:.2f}\033[m \nAcrescimo: \033[1;34m{:.2f}\033[m \nCusto Final: \033[1;34m{:.2f}\033[m \nTotal Parcela: \033[1;34m{:.0f}\033[m'.format(produto, acrescimo, produto + acrescimo, (produto + acrescimo) / qt_parcelas))
 else:
-    print('FAVOR INFORMAR UMAS DAS CONDIÇÕES INFORMADAS ACIMA...')
+    print('\033[1;34mOPÇÃO INVÁLIDA...\033[m\nFAVOR INFORMAR UMAS DAS CONDIÇÕES INFORMADAS ACIMA...')
