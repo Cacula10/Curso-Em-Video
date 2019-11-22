a626c9ccc6100eed44c77ee572311f6534e866d0
Caçula10 <flaviodesouzamarques@gmail.com>
2019-11-21 23:07:40 -0300

    upload

--- ex049.py
+++ ex049.py
@@ -1,3 +1,5 @@
+print('{:=^50}'.format(' \033[1;34mSISTEMA DA CALCULADORA\033[m '))
+print('')
 tabuada = int(input('Digite um número para ver a sua tabuada: '))
 for v in range(1, 11):
     print(tabuada, 'X', v,  '=  {}'.format(tabuada * v))
