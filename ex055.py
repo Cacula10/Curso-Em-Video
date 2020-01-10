c847d59bd1cd024f9b4fc900935bb31a875423bc
Caçula10 <flaviodesouzamarques@gmail.com>
2020-01-10 10:59:27 -0300

    Upload

--- ex055.py
+++ ex055.py
@@ -1,18 +1,19 @@
 # cabeçalho
 
 print('\033[31m<>\033[m' * 35)
-print('{:^70}'.format('\033[32mGORDÃO E FININHO\033[m'))
+print('{:^70}'.format('\033[32mPESADO E LEVE\033[m'))
 print('\033[31m<>\033[m' * 35)
 
-gordao = 0
-fininho = 0
 
-for pessoa in range(1, 5):
-    peso = int(input('Peso da {} pessoa: '.format(pessoa)))
-    if peso > gordao:
-        gordao = peso
-        fininho = peso
-    elif peso < fininho:
-        fininho = peso
-print(gordao)
-print(fininho)
+for p in range(1, 6):
+    peso = float(input('Peso da {} pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}Kg'.format(maior))
+print('O menor peso lido foi de {}Kg'.format(menor))
