21511b2e5f99c96494840613cfc7b902775c3298
Caçula10 <flaviodesouzamarques@gmail.com>
2020-01-11 11:17:19 -0300

    Upload

---
+++ ex058.py
@@ -0,0 +1,23 @@
+# cabeçalho
+print('\033[31m*\033[m' * 35)
+print('{:^40}'.format('\033[32mEXERCICIO 058\033[m'))
+print('\033[31m*\033[m' * 35)
+print('Sou seu computador...')
+print('Acabei de pensar em um numero entre 0 e 10.')
+print('Será que consegue adivinhar qual foi ? ')
+
+# importando bibilioteca
+from random import randint
+sorteio = randint(1, 10)
+
+# programa
+palpite = int(input('Qual o seu palpite ? '))
+while palpite != sorteio:
+    if palpite > sorteio:
+        print('Menos... Tente outra vez.')
+    else:
+        print('Mais... Tente outra vez.')
+    palpite = int(input('Qual o seu palpite? '))
+print(palpite, 'palpite')
+print(sorteio, 'sorteio')
+print('fim')
