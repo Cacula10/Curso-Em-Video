b9b9f25ac615aef41d3b45f329eb5b18dd22c616
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-12 13:33:17 -0300

    Upload

--- ex072.py
+++ ex072.py
@@ -3,6 +3,8 @@ print('{:-^30}'.format('EXERCICIO 072'))
 print('=>' * 15)
 tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 numero = int(input('Digite um numero entre 0 e 20: '))
-while numero > 20 or numero < 0:
+while True:
     numero = int(input('Erro... Favor digitar um numero entre 0 e 20: '))
+    if 0 <= numero <= 20:
+        break
 print(f'O número que você digitou foi: {tupla[numero]}')
