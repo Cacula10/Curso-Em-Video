44765d89dc6a688390747b85ea65961540a6285d
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-15 11:43:38 -0300

    Upload

--- ex077.py
+++ ex077.py
@@ -1,11 +1,11 @@
 print('*' * 30)
 print('{:^30}'.format('EXERCICIO 077'))
 print('*' * 30)
-tupla = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO',
-         'PROGRAMADOR', 'FUTURO')
+tupla = ('Aprender', 'Programar', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO',
+         'Programador', 'Futuro')
 
 for p in tupla: # aqui nós varremos cada palavra da tupla
-    print(f'Na palavra {p} temos:', end='')
+    print(f'Na palavra {p.upper()} temos:', end='')
     for letra in p: # aqui nós varremos cada letra de cada palavra
         if letra in 'AEIOU':
             print('\033[1:31m', letra, '\033[m', end='')
