1979f29fe99830e4ac5366ca442dc2edecc336c2
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-22 15:28:28 -0300

    Upload

--- ex084.py
+++ ex084.py
@@ -1,32 +1,31 @@
-lista = list()
-pilha = list()
+temp = list()
+principal = list()
 maior = menor = 0
 while True:
-    lista.append(str(input('Digite o seu nome: ')))
-    lista.append(float(input('Digite o seu peso: ')))
-    if len(pilha) == 0:
-        maior = menor = lista[1]
+    temp.append(str(input('Digite o seu nome: ')))
+    temp.append(float(input('Digite o seu peso: ')))
+    if len(principal) == 0:
+        maior = menor = temp[1]
     else:
-        if lista[1] > maior:
-            maior = lista[1]
-        if lista[1] < menor:
-            menor = lista[1]
-    pilha.append(lista[:])
-    lista.clear()
-    continuar = ' '
-    while continuar not in 'SN':
-        continuar = str(input('Deseja continuar [SN] ?')).upper()[0]
-    if continuar == 'N':
-        print('<>' * 20)
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Deseja continuar [SN]?')).upper().strip()[0]
+    if resp == 'N':
         break
-print(f'Ao todo, você cadastrou {len(pilha)} pessoas')
-print(f'O maior peso foi de {maior}Kg. Peso de ...', end=' ')
-for p in pilha:
+print(f'Ao todo, você cadastrou {len(principal)} pessoas')
+print(f'O maior peso foi de {maior}kg. Peso de ', end=' ')
+for p in principal:
     if p[1] == maior:
         print(f'{p[0]}', end=' ')
 print('')
-print(f'O menor peso foi de {menor}Kg. Peso de ...', end=' ')
-for p in pilha:
+print(f'O menor peso foi de {menor}Kg. Peso de ', end=' ')
+for p in principal:
     if p[1] == menor:
-        print(f'{p[0]}', end=' ')
+        print(f'{p[0]}',end=' ')
 print('')
