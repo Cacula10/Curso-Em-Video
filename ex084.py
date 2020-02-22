8e452bc5dff3c4761e0d28bbe1fa314106f05399
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-22 14:51:15 -0300

    Upload

--- ex084.py
+++ ex084.py
@@ -1,40 +1,32 @@
 lista = list()
-temp = []
-maiorp = menorp = 0
-
+pilha = list()
+maior = menor = 0
 while True:
-    temp.append(str(input('Informe o seu nome: ')))
-    temp.append(float(input('Informe o seu peso: ')))
-    if len(lista) == 0:
-        maiorp = temp[1]
-        menorp = temp[1]
+    lista.append(str(input('Digite o seu nome: ')))
+    lista.append(float(input('Digite o seu peso: ')))
+    if len(pilha) == 0:
+        maior = menor = lista[1]
     else:
-        if temp[1] > maiorp:
-            maiorp = temp[1]
-        if temp[1] < menorp:
-            menorp = temp[1]
-    r = str(input('Deseja continuar [S/N]?'))
-    lista.append(temp[:])
-    del(temp[:])
-    if r in 'Nn':
+        if lista[1] > maior:
+            maior = lista[1]
+        if lista[1] < menor:
+            menor = lista[1]
+    pilha.append(lista[:])
+    lista.clear()
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Deseja continuar [SN] ?')).upper()[0]
+    if continuar == 'N':
+        print('<>' * 20)
         break
-
-
-print(f'Ao todo você cadastrou {len(lista)} pessoas')
-
-print('{:-^40}'.format('TESTE DE FORMATAÇÃO COM PRINT'))
-
-print(f'O maior peso foi {maiorp:.0f} e pertence > ',end=' ')
-for peso in lista:
-    if peso[1] == maiorp:
-        print(f' ',peso[0],'...',end='')
-print()
-
-print('{:-^40}'.format('TESTE DE FORMATAÇÃO COM PRINT'))
-
-print(f'O menor peso foi {menorp:.0f} e pertence > ', end=' ')
-for peso in lista:
-    if peso[1] == menorp:
-        print(peso[0],'...',end='')
-print()
-print('{:-^40}'.format('FIM DE PROGRAMA'))
+print(f'Ao todo, você cadastrou {len(pilha)} pessoas')
+print(f'O maior peso foi de {maior}Kg. Peso de ...', end=' ')
+for p in pilha:
+    if p[1] == maior:
+        print(f'{p[0]}', end=' ')
+print('')
+print(f'O menor peso foi de {menor}Kg. Peso de ...', end=' ')
+for p in pilha:
+    if p[1] == menor:
+        print(f'{p[0]}', end=' ')
+print('')
