91c0607e5b2c2c5f807fa022d5f6a216083cfaa8
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-23 16:29:29 -0300

    Upload

--- ex086.py
+++ ex086.py
@@ -1,10 +1,15 @@
-matriz = [0,0,0],[0,0,0],[0,0,0]
-
-for l in range(0,3):
-    for c in range(0,3):
-        matriz[l][c] = int(input(f'Digite um valor para [{l}][{c}]: '))
-
-for l in range(0,3):
-    for c in range(0,3):
-        print(matriz[l][c],end=' ')
-    print()
+print('*' * 15)
+print('{:^15}'.format('EXERCICIO 086'))
+print('*' * 15)
+matriz = [[], [], []]
+for pos in range(0, 3):
+    n = int(input(f'Digite um valor para {0} {pos}: '))
+    matriz[0].append(n)
+for pos in range(0, 3):
+    n = int(input(f'Digite um valor para {1} {pos}: '))
+    matriz[1].append(n)
+for pos in range(0, 3):
+    n = int(input(f'Digite um valor para {2} {pos}: '))
+    matriz[2].append(n)
+for i, v in enumerate(matriz):
+    print(v)
