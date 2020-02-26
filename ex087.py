681dd2883982be0abd62f3f4d0573707414e8607
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-26 19:33:41 -0300

    Upload

--- ex087.py
+++ ex087.py
@@ -1,35 +1,32 @@
-matriz = [0,0,0],[0,0,0],[0,0,0]
-soma = somacoluna = segundalinha = somapares = 0
+print('*' * 25)
+print('{:^35}'.format('\033[1:32mEXERCICIO 087\033[m'))
+print('*' * 25)
+par = terceiracoluna = maior = 0
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-for l in range(0,3):
-    for c in range(0,3):
-        matriz[l][c] = int(input(f'Digite um valor para [{l}][{c}]: '))
-        soma += matriz[l][c]
-        somacoluna += matriz[l][2]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Informe um numero: {l} {c}: '))
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end=' ')
+    print('')
+
+for l in range(0, 3):
+    terceiracoluna += matriz[l][2]
+    for c in range(0, 3):
         if matriz[l][c] % 2 == 0:
-            somapares += matriz[l][c]
+            par += matriz[l][c]
 
-for c in range(0, 3):
-    for l in range(0, 3):
-        if l == 0:
-            segundalinha = matriz[1][l]
+for l in range(0, 1):
+    for c in range(0, 3):
+        if c == 0:
+            maior = matriz[1][c]
         else:
-            if matriz[1][l] > segundalinha:
-                segundalinha = (matriz[1][l])
-
-for l in range(0,3):
-    for c in range(0,3):
-        print(matriz[l][c],end=' ')
-    print()
-
-
-print('{:-^40}'.format('RESPOSTAS'))
-print(f'A soma de todos os valores é {soma}')
-print('-'* 40)
-print(f'A soma dos valores pares é: {somapares}')
-print('-'*40)
-print(f'A soma da terceira coluna é: {somacoluna}')
-print('-'*40)
-print(f'O maior valor da segunda linha é: {segundalinha}')
-print()
-print('{:-^40}'.format('FIM DO PROGRAMA'))
+            if matriz[1][c] > maior:
+                maior = matriz[1][c]
+print('\033[1:32m*\033[m' * 25)
+print(f'A soma dos valores pares é \033[1:31m{par}\033[m')
+print(f'A soma dos valores da terceira coluna é \033[1:31m{terceiracoluna}\033[m')
+print(f'A maior valor da segunda linha é: \033[1:31m{maior}\033[m')
