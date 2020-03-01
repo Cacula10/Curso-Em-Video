d945dbe045d7ade8e8bfd3ae3c6e93c8efcc4d8d
Caçula10 <flaviodesouzamarques@gmail.com>
2020-03-01 18:10:29 -0300

    Upload

--- ex090.py
+++ ex090.py
@@ -1,16 +1,17 @@
 print('-' * 25)
 print('{:^25}'.format('EXERCICIO 90'))
 print('-' * 25)
-nome = str(input('Nome: '))
-media = float(input(f'Média do {nome}: '))
-situacao = ' '
-if media >= 6:
-    situacao = 'Aprovado'
+alunos = dict()
+alunos['Nome'] = str(input('Nome: '))
+alunos['Media'] = float(input(f'Média do {alunos["Nome"]}: '))
+if alunos['Media'] >= 7:
+    alunos['Situacao'] = 'Aprovado'
+elif 5 <= alunos['Media'] <= 7:
+    alunos['Situacao'] = 'Exame'
 else:
-    situacao = 'Reprovado'
-dicionario = {'Nome': nome, 'Média': media, 'Situação': situacao}
+    alunos['Situacao'] = 'Reprovado'
 print('-' * 25)
-for k, v in dicionario.items():
+for k, v in alunos.items():
     print(f'\033[1:31m{k:<10}\033[m é igual a \033[1:32m{v:>10}\033[m')
 
 
