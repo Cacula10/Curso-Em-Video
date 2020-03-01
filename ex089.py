0d54dabdef29e6023284398dd05d314475283941
Caçula10 <flaviodesouzamarques@gmail.com>
2020-02-29 21:55:02 -0300

    Upload

--- ex089.py
+++ ex089.py
@@ -1,27 +1,40 @@
-lista = list()
-
+print('-' * 25)
+print('{:^30}'.format('\033[31mBOLETIM DE NOTAS\033[m'))
+print('-' * 25)
+lista = []
+notas = []
+boletim = []
 while True:
-    nome = str(input('Informe o seu nome: '))
-    nota1 = float(input('Informe a sua nota1: '))
-    nota2 = float(input('Informe a sua nota2: '))
-    media = (nota1 + nota2) / 2
-    lista.append([nome, [nota1,nota2],media])
-    resp = str(input('Deseja continuar ? [S/N]'))
-    if resp in 'Nn':
-        print('FIM DE PROGRAMA...')
+    nome = str(input('Digite o seu nome: '))
+    lista.append(nome)
+    nota = int(input('Digite a sua nota 01: '))
+    notas.append(nota)
+    nota = int(input('Digite a sua nota 02: '))
+    notas.append(nota)
+    media = (notas[0] + notas[1]) / 2
+    lista.append(notas[:])
+    lista.append(media)
+    boletim.append(lista[:])
+    notas.clear()
+    lista.clear()
+    continua = ' '
+    while continua not in 'SN':
+        continua = str(input('Deseja continuar ?')).upper()[0]
+    if continua == 'N':
         break
-print('-=' * 30)
-print(f'{"Cod.":<5}{"Nome":<10}{"Media":>15}')
-print('-=' * 25)
+print('-' * 40)
 
-for i, n in enumerate(lista):
-    print(f'{i:<5}{n[0]:<10}{n[2]:>15}')
-print('*' * 25)
+print('{:<10}'.format('ÍNDICE'), end=' ')
+print('{:^15}'.format('NOME'), end=' ')
+print('{:>10}'.format('MÉDIA', end=' '))
+print()
+for i, v in enumerate(boletim):
+    print(f'{i} {v[0]:^33} {v[2]}', end=' ')
+    print()
+print('-' * 40)
+opcao = 0
 while True:
-    opc = int(input('Digite o Cod. para notas ou [999] para encerrar: '))
-    if opc == 999:
-        print('FIM DE PROGRAMA')
+    opcao = int(input('Mostrar nota de qual aluno ? (999 para interromper): '))
+    if opcao == 999:
         break
-    if opc <= len(lista)-1:
-        print(f'As notas de {lista[opc][0]} são {lista[opc][1]}')
-print(lista)
+    print(f'As notas do {boletim[opcao][0]} são {boletim[opcao][1]}')
