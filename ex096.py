45aa92c194a9a880ff2cfcc416b1526c14ea484b
Caçula10 <flaviodesouzamarques@gmail.com>
2020-03-03 20:51:16 -0300

    Upload

--- ex096.py
+++ ex096.py
@@ -1,11 +1,13 @@
-def area(largura,comprimento):
-    area = larg * comp
+print('-' * 30)
+print('{:^30}'.format('EXERCICIO 96'))
+print('-' * 30)
+def área(l, c):
+    area = l * c
+    print(f'A área de um terreno {l}x{c} é de {area} metros quadrado')
 
+# PROGRAMA PRINCIPAL
 print('Controle de Terrenos')
-print('*' * 25)
-larg = float(input('LARGURA (m): '))
-comp = float(input('COMPRIMENTO (m): '))
-print(f'A área de um terreno é: {larg}x{comp} é de {larg*comp}m2')
-
-area(3, 3)
-
+print('--------------------')
+largura = float(input('Largura: '))
+comprimento = float(input('Comprimento: '))
+área(largura, comprimento)
