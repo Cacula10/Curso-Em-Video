a9632603edf61777a9f5c4b219f4e59438f30615
Caçula10 <flaviodesouzamarques@gmail.com>
2020-03-04 21:37:50 -0300

    upload

---
+++ ex100.py
@@ -0,0 +1,23 @@
+from random import randint
+randint(1, 10)
+lista = list()
+listapares = list()
+
+
+def sorteio(num):
+    for v in range(1, num+1):
+        lista.append(randint(1, 10))
+
+
+def pares():
+    soma = 0
+    for v in lista:
+        if v % 2 == 0:
+            soma += v
+    return soma
+
+# Programa principal
+
+sorteio(4)
+print(f'Sorteando {len(lista)} valores da lista: {lista}')
+print(f'Somando os valores pares da lista {lista}, temos o resultado {pares()}')
