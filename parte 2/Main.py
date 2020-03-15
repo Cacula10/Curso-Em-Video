c1eb2e008f139f55d08992ccca52ee667920a058
Caçula10 <flaviodesouzamarques@gmail.com>
2020-03-15 19:44:41 -0300

    upload

---
+++ parte 2/Main.py
@@ -0,0 +1,6 @@
+import moeda
+
+preço = float(input('Digite o preço R$: '))
+print(f'A metade de R${preço} é R${moeda.metade(preço)}')
+print(f'O dobro de R${preço} é R${moeda.dobro(preço)}')
+print(f'Aumentando 10% de R${preço} temos R${moeda.aumentar(preço, taxa=0.10)} ')
---
+++ parte 2/moeda.py
@@ -0,0 +1,19 @@
+def aumentar(numero, taxa):
+    resp = (numero * taxa) + numero
+    return resp
+
+
+def diminuir(numero, taxa):
+    resp = (numero * taxa) - numero
+    return resp
+
+
+def dobro(numero):
+    resp = numero * 2
+    return resp
+
+
+def metade(numero):
+    resp = numero/2
+    return resp
+
