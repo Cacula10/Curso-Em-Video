975321e2b4308ae2789ebaa06ad04c95e49a5619
Caçula10 <flaviodesouzamarques@gmail.com>
2020-03-26 10:53:24 -0300

    upload

---
+++ ex110/moeda.py
@@ -0,0 +1,47 @@
+def metade(valor=0, formatacao=False):
+    resp = valor / 2
+    return resp if formatacao is False else moeda(resp)
+
+
+def dobro(valor=0, formatacao=False):
+    resp = valor * 2
+    return resp if formatacao is False else moeda(resp)
+
+
+def aumentar(valor=0, taxa=0, formatacao=False):
+    resp = valor + (valor * taxa)
+    return resp if formatacao is False else moeda(resp)
+
+
+def diminuir(valor=0, taxa=0, formatacao=False):
+    resp = valor - (valor * taxa)
+    return resp if formatacao is False else moeda(resp)
+
+
+def moeda(valor=0, moeda='R$ '):
+    """
+    :param valor: Nesse parametro é o valor digitado pelo usuário na variavel preço
+    :param moeda: Parametro que eu fixo o R$
+    :return:  Retorno o preço formatado com R$ + ',' ao invez do '.'
+    """
+    return f'{moeda}{valor:.2f}'.replace('.', ',')
+
+
+def resumo(valor, aumento=0, diminui=0):
+    """
+    :param valor:  Preço digitado pelo usuario
+    :param aumento: Taxa de aumento fixada em 10%
+    :param diminui: Taxa de dimunuição fixada em 20%
+    :return: não retorna nada... somente tabulamos e criamos um resumo de todas as "def"
+    """
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço Analisado: \t{moeda(valor)}')
+    print(f'Dobro do Preço: \t{dobro(valor, True)}')
+    print(f'Metade do Preço: \t{metade(valor, True)}')
+    print(f'10% de Aumento: \t{aumentar(valor, aumento/100, True)}')
+    print(f'20% de Redução: \t{diminuir(valor, diminui/100, True)}')
+    print('-' * 30)
+
+
